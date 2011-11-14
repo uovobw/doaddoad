7f65ee03d5c36113fa60bb3f5840fea2e8bdc43d
Andrea Lusuardi <linolusu@tin.it>
2011-11-14 22:07:50 +0000

    First commit. Missing: 	- cld library with python bindings 	- python-twitter 	- secrets for the aws application

---
+++ doaddoad.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python2.7
+
+import twitter
+import subprocess
+import cld
+import cPickle as pickle
+import re
+import os
+import time
+import secrets
+
+class DadaDodo(object):
+	def __init__(self):
+		self.dadadodo = ["/usr/bin/dadadodo","-c","1","-"]
+		self.thisUser = "doaddoad"
+		self.twitterLog = "twitter.log"
+		self.lang = "en"
+
+		self.consumer_key = secrets.consumer_key
+		self.consumer_secret = secrets.consumer_secret
+		self.access_token_key =	secrets.access_token_key
+		self.access_token_secret = secrets.access_token_secret
+
+		self.api = twitter.Api(consumer_key=self.consumer_key,consumer_secret=self.consumer_secret,access_token_key=self.access_token_key,access_token_secret=self.access_token_secret)
+		self.user = None
+
+		try:
+			user = self.api.VerifyCredentials()
+			self.user = user
+		except TwitterError, e:
+			print e
+			print "ERROR in authenticating to twitter"
+			sys.exit(123)
+
+		self.lang = "en"
+		self.state = self.loadState()
+		self.stateLimit = 15000
+
+
+	def launch(self,strInput):
+		dadadodo =	subprocess.Popen(self.dadadodo,stdin=subprocess.PIPE,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+		out, err = dadadodo.communicate(strInput)
+		return out
+
+	def cldDetect(self,string):
+		return cld.detect(string,isPlainText=True,includeExtendedLanguages=False)
+
+	def isEnglish(self,string):
+		try:
+			topLanguageName, topLanguageCode, isReliable, textBytesFound, details =	self.cldDetect(string)
+		except UnicodeEncodeError, e:
+			#print "Cannot detect language of \"%s\"" % (string)
+			print "errored"
+			topLanguageCode = "nolang"
+		return topLanguageCode == self.lang
+
+	def loadState(self):
+		state = None
+		fd = open(self.twitterLog,"r")
+		try:
+			state = pickle.load(fd)
+		except pickle.UnpicklingError, e:
+			print "Error in unpickling: " + e
+		return state
+
+	def saveState(self):
+		try:
+			pickle.dump(self.state,open(self.twitterLog,"w"),-1)
+		except pickle.PicklingError, e:
+			print "Error in pickling: " + e
+
+	def updateState(self):
+		if self.state:
+			# get all the followers for doaddoad
+			allFollowers = self.api.GetFollowers()
+			for each in allFollowers:
+				print "Fetching timeline for user %s (@%s)" % (each.GetName(),each.GetScreenName())
+				# get the timeline for the user
+				timeline = self.api.GetUserTimeline(id=each.GetId(),count=20)
+				# add all the Status objects to the current status
+				for status in timeline:
+					if self.isEnglish(status.GetText()):
+						self.state.append(status)
+			if len(self.state) > self.stateLimit:
+				self.state = self.state[:-(self.stateLimit)]
+		else:
+			print "FATAL: we got to update stage with an empty state!"
+			sys.exit(124)
+	
+	def handleRt(self,string):
+		pattern = re.compile(r"(^.*)([Rr][Tt]) +(\S+)\s(.*)$")
+		matcher = pattern.match(string)
+		if matcher:
+			string = "RT @%s %s%s" %	(matcher.group(3),matcher.group(1),matcher.group(4))
+		return string
+
+	def generateTweets(self):
+		data = " ".join([ x.GetText() for x in self.state])
+		out = self.launch(re.sub(r'\s', ' ', data.encode("ascii","ignore")))
+		# remove tabulation
+		out.replace("\t"," ")
+		# strip leading whitespaces
+		out.strip(" \t")
+		if len(out) > 140:
+			out = out[:140]
+			out = out[:out.rindex(" ")]
+		out = self.handleRt(out)
+		return out.replace("\n","")
+		
+
+if __name__ == "__main__":
+	d = DadaDodo()
+	lastUpdateTime = os.stat(d.twitterLog).st_mtime
+	saveState = False
+	if lastUpdateTime <= (time.time() - 7200):
+		d.updateState()
+		saveState =  True
+	d.api.PostUpdate(d.generateTweets())
+	if saveState:
+		d.saveState()
+
+
+	
