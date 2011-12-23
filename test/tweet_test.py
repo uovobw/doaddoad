f1b9187ab6857e269ae9bd73a9175f7f82d9ba48
Filippo Giunchedi <filippo@esaurito.net>
2011-12-23 12:31:53 +0100

    use keys() and values()

--- test/tweet_test.py
+++ test/tweet_test.py
@@ -17,5 +17,5 @@ def test_load_state():
 	# assert it's a dict
 	assert type(d.state) == dict
 	# assert it's structure
-	assert type(d.state.keys[0]) == int
-	assert type(d.state.values[0]) == Tweet
+	assert type(d.state.keys()[0]) == int
+	assert type(d.state.values()[0]) == Tweet
