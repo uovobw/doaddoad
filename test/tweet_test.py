ecb8452caab9cb421b0a0d088fd3cd616f767374
Andrea Lusuardi <linolusu@tin.it>
2011-12-23 14:03:33 +0000

    Separated tweet_test and doaddoad_test

--- test/tweet_test.py
+++ test/tweet_test.py
@@ -11,11 +11,3 @@ def test_tweet_creation():
 	assert doaddoadTweet.get_language_code() == "en"
 
 
-def test_load_state():
-	d = DoadDoad()
-	d.load_state()
-	# assert it's a dict
-	assert type(d.state) == dict
-	# assert it's structure
-	assert type(d.state.keys()[0]) == int
-	assert type(d.state.values()[0]) == Tweet
