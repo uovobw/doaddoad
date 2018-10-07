5676096c242d48acb2b33f98582c68753ba08045
Filippo Giunchedi <fgiunchedi@wikimedia.org>
2018-10-07 13:40:45 +0200

    Add setup.py and tox.ini
    Also remove now-duplicated AUTHORS

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='doaddoad',
+      version='0.666',
+      description='Python Distribution Utilities',
+      author='Filippo Giunchedi / Andrea Lusuardi',
+      url='https://github.com/uovobw/doaddoad',
+     )
