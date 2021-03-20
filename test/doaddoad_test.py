53a0616e83040a5e93e19f568c6801b6e9066d22
Filippo Giunchedi <filippog@users.noreply.github.com>
2021-03-20 17:13:04 +0100

    Fix bytes/str encoding, plus tests (#2)
    Co-authored-by: Filippo Giunchedi <filippo@debian.org>

--- test/doaddoad_test.py
+++ test/doaddoad_test.py
@@ -1,8 +1,31 @@
-from doaddoad import DoadDoad
+import random
+import time
+import unittest
+
+import twitter
+from doaddoad import DoadDoad, Tweet
 
 
-def test_load_empty_state():
-	d = DoadDoad()
-	d.load_state()
-	# assert it's a dict
-	assert type(d.state) == dict
+class DoaddoadStateTest(unittest.TestCase):
+    def setUp(self):
+        self.d = DoadDoad()
+
+    def _add_tweet(self, text):
+        created_at = time.time()
+        id = random.randint(0, int(created_at))
+        self.d.state[id] = Tweet(
+            twitter.Status.NewFromJsonDict(
+                {"id": id, "created_at": created_at, "text": text}
+            )
+        )
+
+    def test_load_empty_state(self):
+        self.d.load_state()
+        assert self.d.state == {}
+
+    def test_generate_tweet(self):
+        self._add_tweet("lorem ipsum dolor sit amet")
+        t = self.d.generate_tweet()
+        assert len(t) > 10
+        assert "  " not in t
+        assert "\n" not in t
